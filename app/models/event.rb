368fddd035f62f02f58711f989e2b1a5b84b3769
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-11-23 16:36:04 +0900

    バリデーション設定

---
+++ app/models/event.rb
@@ -0,0 +1,23 @@
+class Event < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :delivery_area
+  belongs_to :time_all
+  
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :delivery_area_id
+    validates :time_all_id
+  end
+
+  with_options presence: true do
+    validates :product
+    validates :introduction
+    validates :price, numericality: { greater_than_or_equal_to: 200, less_than_or_equal_to: 99_999 },
+                      format: { with: /\A[0-9]+\z/ }
+    validates :start
+  end  
+end
